bd2ddc53971a45ce054ff5424819e0df15aabb9d
Tianshi Li <9413fi@gmail.com>
2017-05-19 17:32:22 -0400

    change back to sync storage

--- extension/background.js
+++ extension/background.js
@@ -16,8 +16,14 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     tabList.push(tab)
     tabDict[tabId] = tabList.length-1
     tabHistory.push(new TabRecord(tabDict[tabId], (new Date()).getTime()))
-    chrome.runtime.sendMessage("mpkbfmdhciepiffhidjiloldcfejkebc", {"tabActivated": tabHistory[tabHistory.length-1]})
-    chrome.runtime.sendMessage("mpkbfmdhciepiffhidjiloldcfejkebc", {"newTab": {"id":tabList.length-1, "tab": tab}})
+//     chrome.runtime.sendMessage("mpkbfmdhciepiffhidjiloldcfejkebc", {"tabActivated": tabHistory[tabHistory.length-1]})
+//     chrome.runtime.sendMessage("mpkbfmdhciepiffhidjiloldcfejkebc", {"newTab": {"id":tabList.length-1, "tab": tab}})
+    tabActivatedRecord = {}
+    tabActivatedRecord["tabActivated-" + (tabHistory.length-1)] = tabHistory[tabHistory.length-1]
+    chrome.storage.sync.set(tabActivatedRecord)
+    tabListRecord = {}
+    tabListRecord["newTab-" + (tabList.length-1)] = tab
+    chrome.storage.sync.set(tabListRecord)
   }
 })
 
@@ -25,6 +31,9 @@ chrome.tabs.onActivated.addListener(function(activeTab) {
   if (typeof tabDict[activeTab.tabId] !== "undefined") {
     console.log(tabDict[activeTab.tabId])
     tabHistory.push(new TabRecord(tabDict[activeTab.tabId], (new Date()).getTime()))
-    chrome.runtime.sendMessage("mpkbfmdhciepiffhidjiloldcfejkebc", {"tabActivated": tabHistory[tabHistory.length-1]})
+//     chrome.runtime.sendMessage("mpkbfmdhciepiffhidjiloldcfejkebc", {"tabActivated": tabHistory[tabHistory.length-1]})
+    tabActivatedRecord = {}
+    tabActivatedRecord["tabActivated-" + (tabHistory.length-1)] = tabHistory[tabHistory.length-1]
+    chrome.storage.sync.set(tabActivatedRecord)
   }
 });
